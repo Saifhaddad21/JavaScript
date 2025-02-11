d1a68dd43e5faf0f82a4767c80b6d36c86946240
saif <saif>
2025-02-11 22:02:26 +0300

    var

--- main.js
+++ main.js
@@ -1,46 +1,19 @@
-// Output To Screen
-//    Window.alert()
-//    document.write()
-//    console.log()
+// variable intro 
+// what is variable ??
+// why we use variables ??
+// declare a variable and use 
+// syntax ( keyword || variable name ||assignment operator || variable value )
+// variable without var 
+// multiple variable in the same line 
+// id and global variable 
+// loosely typed vs strongly typed
 
-//    syntax
+var user = "saif" , 
+age = 24 ;
+console.log(user);
+console.log(user);
+console.log(user);
+console.log(age);
+console.log(hello);
 
-// window.alert("Hello From JS File");
-// document.write("<h1>Hello <span>Page</span></h1>");
-// console.log("Hello From JS File");
-// console.log("Hello From JS Files");
-
-// Console Methods
-//   log
-//   error
-//   table
-
-//   Web API
-
-//   Styling Console
-//      Directive %c
-
-// console.log("log");
-// console.error("error");
-// console.table(["saif" , "jack" , "jiries"]);
-
-// console.log("Hello From %cJS %CFile", "color:red; font-size:40px", "color:blue; font-size:40px");
-
-
-//  ES6
-// var  myName = "Saif";
-
-// console.log("Hello " + myName);
-// console.log(`Hello` &{myName});
-
-console.log("Saif Haddad");                               
-console.log(typeof "Saif Haddad");
-console.log(typeof 5000);
-console.log(typeof 5000.99);
-console.log(typeof [10, 15, 17]);
-console.log(typeof ["sa", "za", "ja"]);
-console.log(typeof {name: "saif" , age: "24" , contry: "jordan"});
-console.log(typeof true);
-console.log(typeof false);
-console.log(typeof undefined);
-console.log(typeof null);
+hello.innerHTML = "option";
