9aecaac23f70fc62a93d45c9c5f190788c41ce47
saif <saif>
2025-02-11 23:46:55 +0300

    template

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 
 // template literals (template string)
 
-let a = "We Love";
-let b = "JavaScript";
-let c = "and"
-let d = "programming"
+let title = "Elzero";
+let desc = "Elzero Web School";
 
-console.log(a + " \"\" " + b + " " + "\n" + c + " " + d);
-
-console.log(`${a} "" '' \\ ${b} 
-    ${c} ${d}`)
-
+let markUp = 
+<div class="card">
+    <div class="child">
+        <h2>${title}</h2>
+        <p>${desc}</p>
+    </div>
+</div>
+;
+document.write(markUp);
