741fa31a5c8338bd64d787d022d3964dbcbe8aff
saif <saif>
2025-02-11 21:45:14 +0300

    console

---
+++ main.js
@@ -0,0 +1,46 @@
+// Output To Screen
+//    Window.alert()
+//    document.write()
+//    console.log()
+
+//    syntax
+
+// window.alert("Hello From JS File");
+// document.write("<h1>Hello <span>Page</span></h1>");
+// console.log("Hello From JS File");
+// console.log("Hello From JS Files");
+
+// Console Methods
+//   log
+//   error
+//   table
+
+//   Web API
+
+//   Styling Console
+//      Directive %c
+
+// console.log("log");
+// console.error("error");
+// console.table(["saif" , "jack" , "jiries"]);
+
+// console.log("Hello From %cJS %CFile", "color:red; font-size:40px", "color:blue; font-size:40px");
+
+
+//  ES6
+// var  myName = "Saif";
+
+// console.log("Hello " + myName);
+// console.log(`Hello` &{myName});
+
+console.log("Saif Haddad");                               
+console.log(typeof "Saif Haddad");
+console.log(typeof 5000);
+console.log(typeof 5000.99);
+console.log(typeof [10, 15, 17]);
+console.log(typeof ["sa", "za", "ja"]);
+console.log(typeof {name: "saif" , age: "24" , contry: "jordan"});
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
