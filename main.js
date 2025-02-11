e81e6874edb7d13fb9e4d3c3bb05a500be6a202f
saif <saif>
2025-02-11 23:14:03 +0300

    back\slash

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
-// Var :
-// 1-Redeclar (Yes)
-// 2-Access Before Declare (undefined)
-// 3-Variable Scope Drama [Added To Window] ()
-// 4-Block Or Function Scope ()
 
-// Let : 
-// 1-Redeclar (No => Error)
-// 2-Access Before Declare (error)
-// 3-Variable Scope 
-// 4-Block Or Function Scope 
+// String Syntax + Character Escape Sequences
+// \Escape + Line Continue 
+// \n 
 
-// Const : 
-// 1-Redeclar (No => Error)
-// 2-Access Before Declare (error)
-// 3-Variable Scope 
-// 4-Block Or Function Scope 
-
-var a = 1 ;
-var a = 1 ;
-
-let a = 1 ; 
-let a = 1 ;
-console.log(a);
+console.log('elzero web "school"');
+console.log("elzero web 'school'");
+console.log("elzero web \"school\"");
+console.log('elzero \\ web \'school\'');
+console.log("elzero \
+    web \
+    school");
+    console.log("elzero\nweb\nschool");
