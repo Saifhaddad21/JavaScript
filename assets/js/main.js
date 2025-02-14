94d268a4254b818ddfca0e58250ebb733d9575de
saif <saif>
2025-02-14 21:08:27 +0300

    Lexical(Static)

--- assets/js/main.js
+++ assets/js/main.js
@@ -289,3 +289,22 @@
 
 
 ///////////////////////
+
+// (Scope-Lexical(Static))
+
+function parent() {
+    let a = 10;
+
+    function child() {
+        console.log(a);
+
+        function grand() {
+            let b = 200;
+            console.log(`From Grand ${a}`)
+            console.log(`From Grand ${b}`)
+        }
+        grand();
+    }
+    child();
+}
+parent();
