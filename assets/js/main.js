610a1dc1d2945c1452de83998fa51162768cedf9
saif <saif>
2025-02-15 02:04:16 +0300

    Reduce Practice

--- assets/js/main.js
+++ assets/js/main.js
@@ -468,15 +468,30 @@ Named Function
 //     Index => The index of the current element being processed in the array.. 
 //     Starts from index 0 if an initialValue is provided.. 
 //     Otherwise, it starts from index 1. 
-// Array => The Current Array
-let nums = [10, 20, 15, 30];
-let add = nums.reduce(function (acc, current, index, arr) {
+// // Array => The Current Array
+// let nums = [10, 20, 15, 30];
+// let add = nums.reduce(function (acc, current, index, arr) {
+//     console.log(`Acc => ${acc}`);
+//     console.log(`Current Element => ${current}`);
+//     console.log(`Current Element Index => ${index}); 
+//     console.log('Array => ${arr}`);
+//     console.log(acc + current);
+//     console.log(`#############`);
+//     return acc + current;
+// }, 5);
+// console.log(add);
+
+// ///////////////////////////////////////////////////////////////////////////////////////
+// (Higher Order Functions - Reduce Practice)
+
+
+let theBiggest = ["Bla", "Propaganda", "Other", "AAA", "Battery", "Test"];
+let check = theBiggest.reduce(function (acc, current) {
     console.log(`Acc => ${acc}`);
     console.log(`Current Element => ${current}`);
-    console.log(`Current Element Index => ${index}); 
-    console.log('Array => ${arr}`);
-    console.log(acc + current);
+    console.log(acc.length > current.length ? acc : current);
     console.log(`#############`);
-    return acc + current;
-}, 5);
-console.log(add);
+    return acc.length > current.length ? acc : current;
+});
+
+console.log(check);
