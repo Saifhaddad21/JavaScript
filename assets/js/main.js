0ed6259668992a59216b9bd8783af551852e8b83
saif <saif>
2025-03-07 03:10:24 +0300

    /

--- assets/js/main.js
+++ assets/js/main.js
@@ -1036,37 +1036,41 @@ clearTimeout(Identifier)
 /////////////////////////////////////////////////////////////////////////////
 // local storage color application practice :
 
-let lis = document.querySelectorAll("ul li");
-let exp = document.querySelector(".experiment");
+// let lis = document.querySelectorAll("ul li");
+// let exp = document.querySelector(".experiment");
 
-// window.localStorage.clear(); 
+// // window.localStorage.clear(); 
 
-if (window.localStorage.getItem("color") ) {
-   // if there is color in local storage :
-   // [1] add color to div 
-  exp.style.backgroundColor = window.localStorage.getItem("color");
-  // [2]  // remove activae class from all lis : 
-    lis.forEach((li) => {
-      li.classList.remove("active"); 
-    });
-    // [3] add active class to current color :
-    document.querySelector(`[data-color="${window.localStorage.getItem("color")}"]`).classList.add("active");
-}else { // if there is no color in local storage :
-  console.log("No");
-};
+// if (window.localStorage.getItem("color") ) {
+//    // if there is color in local storage :
+//    // [1] add color to div 
+//   exp.style.backgroundColor = window.localStorage.getItem("color");
+//   // [2]  // remove activae class from all lis : 
+//     lis.forEach((li) => {
+//       li.classList.remove("active"); 
+//     });
+//     // [3] add active class to current color :
+//     document.querySelector(`[data-color="${window.localStorage.getItem("color")}"]`).classList.add("active");
+// }else { // if there is no color in local storage :
+//   console.log("No");
+// };
 
-lis.forEach((li) => {
-  li.addEventListener("click", (e) =>{ 
-    // console.log(e.currentTarget.dataset.color);
-    // remove activae class from all lis : 
-    lis.forEach((li) => {
-      li.classList.remove("active"); 
-    });
-    // add active class to current element :
-    e.currentTarget.classList.add("active");
-    // add current color to local storge :
-    window.localStorage.setItem("color", e.currentTarget.dataset.color);
-    // change div background color : 
-    exp.style.backgroundColor = e.currentTarget.dataset.color;
-  });
-}); 
+// lis.forEach((li) => {
+//   li.addEventListener("click", (e) =>{ 
+//     // console.log(e.currentTarget.dataset.color);
+//     // remove activae class from all lis : 
+//     lis.forEach((li) => {
+//       li.classList.remove("active"); 
+//     });
+//     // add active class to current element :
+//     e.currentTarget.classList.add("active");
+//     // add current color to local storge :
+//     window.localStorage.setItem("color", e.currentTarget.dataset.color);
+//     // change div background color : 
+//     exp.style.backgroundColor = e.currentTarget.dataset.color;
+//   });
+// }); 
+
+////////////////////////////////////////////////////////////////////
+// (Session Storage And Use Cases) : 
+
