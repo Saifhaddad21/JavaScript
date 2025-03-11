a3564f919275ef7c51e852403783755dc3de4443
saif <saif>
2025-03-11 14:26:07 +0300

    Object Meta Data And Descriptor Part 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -2440,35 +2440,64 @@ Class
 --------configurable [Cannot Delete Or Reconfigure] 
  */
 
+// const myObject = {
+//   a: 1,
+//   b: 2,
+// };
+
+// Object.defineProperty(myObject, "c", {
+//   writable : false,
+//   enumerable: true,
+//   configurable: false,
+//   value: 3,
+// });
+
+// Object.defineProperty(myObject, "c", {
+//   writable : false,
+//   enumerable: true,
+//   configurable: true, // Uncaught TypeError: (Cannot redefine property): c  
+//   value: 3,
+// });
+
+// myObject.c = 100;
+
+// console.log(delete myObject.c)
+
+// for (let prop in myObject) {
+//   console.log(prop, myObject[prop]);
+// }
+// console.log(myObject);
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// (Object Meta Data And Descriptor Part 2 )    :   158
+
 const myObject = {
   a: 1,
   b: 2,
 };
 
-Object.defineProperty(myObject, "c", {
-  writable : false,
-  enumerable: true,
-  configurable: false,
-  value: 3,
+Object.defineProperties(myObject, {
+  c: {
+    configurable: true,
+    value: 3,
+  },
+
+  d: {
+    configurable: true,
+    value: 4,
+  },
+
+  e: {
+    configurable: true,
+    value: 5,
+  },
 });
 
-Object.defineProperty(myObject, "c", {
-  writable : false,
-  enumerable: true,
-  configurable: true, // Uncaught TypeError: (Cannot redefine property): c  
-  value: 3,
-});
-
-myObject.c = 100;
-
-console.log(delete myObject.c)
-
-for (let prop in myObject) {
-  console.log(prop, myObject[prop]);
-}
 console.log(myObject);
 
+console.log(Object.getOwnPropertyDescriptor(myObject, "d"));
 
+console.log(Object.getOwnPropertyDescriptors(myObject));
 
-////////////////////////////////////////////////////////////////////////////////////////////////
-// (Object Meta Data And Descriptor Part 2 )    :   158
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ( Date And Time Introduction) : 159
