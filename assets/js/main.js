e0185c92a892a2b690d1f9d6b9b851d2e78e9172
saif <saif>
2025-03-13 00:52:11 +0300

    JSON Syntax And Compare With JS Object

--- assets/js/main.js
+++ assets/js/main.js
@@ -2904,7 +2904,26 @@ Not Support Comments ------------- === Support Comments  -------------=
 ===========================================================================
 */
 /////////////////////////////////////////////////////////////////////////////////////////
-// (JSON Syntax And Compare With JS Object)   :
+// (JSON Syntax And Compare With JS Object)   : 170
+/*
+JSON Syntax 
+-----Data Added Inside Curly Braces () 
+-----Data Added With Key: Value 
+-----Key Should Be String Wrapped In Double Quotes 
+-----Data Separated By Comma 
+-----Square Brackets: [] For Arrays 
+-----Curly Braces {) For Objects 
+
+Available Data Types 
+-----String 
+-----Number 
+-----Object 
+-----Array 
+-----Boolean Values 
+-----null
+*/
+//////////////////////////////////////////////////////////////////////////////////////////
+// (What Is API) : 171
 
 
 
