c6d8d2426be4271d5c77122eee542d892eb66667
saif <saif>
2025-03-13 01:15:48 +0300

    Parse And Stringify

--- assets/js/main.js
+++ assets/js/main.js
@@ -2933,8 +2933,29 @@ JSON
 // we have web name myjson ..
 /////////////////////////////////////////////////////////////////////////////////
 // (Parse And Stringify) : 172
+/*
+JSON 
+---JSON.parse => Convert Text Data To JS Object 
+    JSON.stringify => Convert JS Object To JSON 
+*/
+const myJsonObjectFromServer = '{"Username": "Saif", "Age": 25}';
+console.log(typeof myJsonObjectFromServer);
+console.log(myJsonObjectFromServer);
+
+const myJsObject = JSON.parse(myJsonObjectFromServer);
+console.log(typeof myJsObject);
+console.log(myJsObject);
+
+myJsObject["Username"] = "Zaid";
+myJsObject["Age"] = 26;
+
+const myJsonObjectToServer = JSON.stringify(myJsObject);
+console.log(typeof myJsonObjectToServer);
+console.log(myJsonObjectToServer);
 
 
+////////////////////////////////////////////////////////////////////////////////
+// (synchronous vs Synchronous Programming)  :  173
 
 
 
